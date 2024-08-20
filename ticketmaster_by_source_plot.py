70b6c70e9472d6402ffbeb90e286dc435c2f8c32
Evan Brown <evan.brown@wmg.com>
2024-08-20 16:28:35 +0100

    Add ticketmaster by source plot

---
+++ ticketmaster_by_source_plot.py
@@ -0,0 +1,22 @@
+# Run data prep script first!
+
+import pandas as pd
+import matplotlib.ticker as mtick
+
+df = pd.read_pickle('combined_df.pkl')
+df['action_date'] = pd.to_datetime(df['Action Date'])
+
+ax = pd.pivot_table(df, values="Action Earnings", index=pd.Grouper(freq='M', key="action_date"), columns=["source"], aggfunc='sum', fill_value=0)
+
+graph = ax.plot(kind='line')
+graph.set_xlabel("Month")
+graph.set_ylabel("Total Earnings")
+graph.set_title('Monthly earnings from Ticketmaster by platform')
+fmt = '${x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+graph.yaxis.set_major_formatter(tick)
+
+fig = graph.get_figure()
+fig.set_tight_layout(True)
+fig.savefig('ticketmaster-by-source.png')
+
