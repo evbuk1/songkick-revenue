6024ef32cd9cf8db01c3056817ab44f5002d6973
Evan Brown <evan.brown@wmg.com>
2024-08-20 21:08:59 +0100

    Add user visitor figure creation

--- ticketmaster_by_source_plot.py
+++ ticketmaster_by_source_plot.py
@@ -6,7 +6,8 @@ import matplotlib.ticker as mtick
 df = pd.read_pickle('combined_df.pkl')
 df['action_date'] = pd.to_datetime(df['Action Date'])
 
-ax = pd.pivot_table(df, values="Action Earnings", index=pd.Grouper(freq='M', key="action_date"), columns=["source"], aggfunc='sum', fill_value=0)
+ax = pd.pivot_table(df, values="Action Earnings", index=pd.Grouper(freq='ME', key="action_date"), columns=["source"], aggfunc='sum', fill_value=0)
+bx = pd.pivot_table(df, values="Action Earnings", index=pd.Grouper(freq='ME', key="action_date"), columns=["login_status"], aggfunc='sum', fill_value=0)
 
 graph = ax.plot(kind='line')
 graph.set_xlabel("Month")
@@ -19,4 +20,16 @@ graph.yaxis.set_major_formatter(tick)
 fig = graph.get_figure()
 fig.set_tight_layout(True)
 fig.savefig('ticketmaster-by-source.png')
+
+graph = bx.plot(kind='line')
+graph.set_xlabel("Month")
+graph.set_ylabel("Total Earnings")
+graph.set_title('Monthly earnings from Ticketmaster by user type')
+fmt = '${x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+graph.yaxis.set_major_formatter(tick)
+
+fig = graph.get_figure()
+fig.set_tight_layout(True)
+fig.savefig('ticketmaster-by-user-type.png')
 
