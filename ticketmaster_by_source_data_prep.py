b70ebe1f6584c2047bb1922123c19b92d8339f47
Evan Brown <evan.brown@wmg.com>
2024-08-20 15:30:41 +0100

    Renamed source data prep script

---
+++ ticketmaster_by_source_data_prep.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def label_source(row):
+    if row['device'] == 'ios' and row['mobile_or_desktop'] == 'app':
+        return 'ios_app'
+    elif row['device'] == 'android' and row['mobile_or_desktop'] == 'app':
+        return 'android_app'
+    elif row['device'] == 'android' or row['device'] == 'ios':
+        return 'mobile_web'
+    elif row['device'] == 'skweb' and row['mobile_or_desktop'] == 'desktop':
+        return 'skweb_desktop'
+    return 'skweb_desktop'
+
+
+impact_df = pd.read_csv('/home/evanbrown/table/impact/combined.csv')
+click_df = pd.read_csv('/home/evanbrown/table/click view/uncompressed/combined.csv')
+
+# Change column name in impact csv first from Sub Id 2 to order_link_id otherwise script will fail
+combined_df = impact_df.merge(click_df, on=['order_link_id'], how='inner')
+
+combined_df['source'] = combined_df.apply(label_source, axis=1)
+
+combined_df.to_pickle('combined_df.pkl')
