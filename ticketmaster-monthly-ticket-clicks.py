5300ae1ba66853a156f2a5933453586b26d62cdc
Evan Brown <evan.brown@wmg.com>
2024-08-21 13:43:46 +0100

    Set number formatters on plot

--- ticketmaster-monthly-ticket-clicks.py
+++ ticketmaster-monthly-ticket-clicks.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.ticker as mtick
 
 df = pd.read_pickle('combined_df.pkl')
 df['action_date'] = pd.to_datetime(df['Action Date'])
@@ -11,6 +11,9 @@ ax = monthly_clicks.plot(kind='line')
 ax.set_xlabel("Month")
 ax.set_ylabel("Number of ticket clicks")
 ax.set_title('Monthly ticket clicks to Ticketmaster vendors - 2022 to 2024')
+fmt = '{x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+ax.yaxis.set_major_formatter(tick)
 
 fig = ax.get_figure()
 fig.set_tight_layout(True)
@@ -23,6 +26,11 @@ yearly_clicks['date'] = yearly_clicks.index.strftime('%Y')
 ax = yearly_clicks.plot(kind='bar')
 ax.set_title('Yearly drop in ticket click numbers - 2023 to 2024')
 ax.set_xticklabels(yearly_clicks['date'])
+fmt = '{x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+ax.yaxis.set_major_formatter(tick)
+ax.set_xlabel("Year")
+ax.set_ylabel("Total ticket clicks")
 fig = ax.get_figure()
 fig.autofmt_xdate()
 fig.set_tight_layout(True)
