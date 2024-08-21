13eafa65cdcb37484e6651a7040bac39ead0c5c7
Evan Brown <evan.brown@wmg.com>
2024-08-21 13:30:40 +0100

    Finish

---
+++ ticketmaster-monthly-ticket-clicks.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib as plt
+
+df = pd.read_pickle('combined_df.pkl')
+df['action_date'] = pd.to_datetime(df['Action Date'])
+df.rename(columns={'category': 'number_of_ticket_clicks'}, inplace=True)
+
+monthly_clicks = df.resample('ME', on='action_date').agg({"number_of_ticket_clicks":'count'})
+
+ax = monthly_clicks.plot(kind='line')
+ax.set_xlabel("Month")
+ax.set_ylabel("Number of ticket clicks")
+ax.set_title('Monthly ticket clicks to Ticketmaster vendors - 2022 to 2024')
+
+fig = ax.get_figure()
+fig.set_tight_layout(True)
+fig.savefig('ticketmaster-ticket-clicks-by-month-2yr.png')
+
+df.rename(columns={'category': 'number_of_ticket_clicks'}, inplace=True)
+yearly_clicks = df.resample('Y', on='action_date').agg({"number_of_ticket_clicks":'count'})
+yearly_clicks = yearly_clicks.iloc[1:]
+yearly_clicks['date'] = yearly_clicks.index.strftime('%Y')
+ax = yearly_clicks.plot(kind='bar')
+ax.set_title('Yearly drop in ticket click numbers - 2023 to 2024')
+ax.set_xticklabels(yearly_clicks['date'])
+fig = ax.get_figure()
+fig.autofmt_xdate()
+fig.set_tight_layout(True)
+fig.savefig('ticketmaster-ticket-clicks-2023-to-2024.png')
