545f9d67382422880e5d6e0aa42dbd1a9366cd43
Evan Brown <evan.brown@wmg.com>
2024-08-24 15:56:56 +0100

    Update monthly clicks

--- ticketmaster_monthly_ticket_clicks.py
+++ ticketmaster_monthly_ticket_clicks.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import matplotlib.ticker as mtick
 
-df = pd.read_pickle('combined_df.pkl')
-df['action_date'] = pd.to_datetime(df['Action Date'])
+df = pd.read_csv('/home/evan/Documents/development/table/click view/without_order_link_id/combined/combined.csv')
+df['action_date'] = pd.to_datetime(df['date'])
 df.rename(columns={'category': 'number_of_ticket_clicks'}, inplace=True)
 
 monthly_clicks = df.resample('ME', on='action_date').agg({"number_of_ticket_clicks":'count'})
 
-ax = monthly_clicks.plot(kind='line')
-ax.set_xlabel("Month")
-ax.set_ylabel("Number of ticket clicks")
-ax.set_title('Monthly ticket clicks to Ticketmaster vendors - 2022 to 2024')
+monthly_clicks_graph = monthly_clicks.plot(kind='line')
+monthly_clicks_graph.set_xlabel("Month")
+monthly_clicks_graph.set_ylabel("Number of ticket clicks")
+monthly_clicks_graph.set_title('Monthly ticket clicks to Ticketmaster vendors - 2022 to 2024')
 fmt = '{x:,.0f}'
 tick = mtick.StrMethodFormatter(fmt)
-ax.yaxis.set_major_formatter(tick)
+monthly_clicks_graph.yaxis.set_major_formatter(tick)
 
-fig = ax.get_figure()
+fig = monthly_clicks_graph.get_figure()
 fig.set_tight_layout(True)
-fig.savefig('ticketmaster-ticket-clicks-by-month-2yr.png')
+fig.savefig('ticketmaster-ticket-clicks-by-month.png')
