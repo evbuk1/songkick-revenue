d0dd4cdef5d6492e2d365ae7765a1932ba5b86b1
Evan Brown <evan.brown@wmg.com>
2024-08-26 14:42:41 +0100

    User type plot update

--- ticketmaster_by_user_type_monthly_plot.py
+++ ticketmaster_by_user_type_monthly_plot.py
@@ -3,20 +3,15 @@
 import pandas as pd
 import matplotlib.ticker as mtick
 
-df = pd.read_pickle('combined_df.pkl')
+df = pd.read_pickle('combined_df_left.pkl')
 df['action_date'] = pd.to_datetime(df['Action Date'])
+df["login_status"].fillna("unattributable", inplace = True)
 
-df2 = pd.read_csv('/home/evan/Documents/development/table/impact/combined.csv')
-unattributed_rows = df2[df2['Sub Id 2'].isna()].copy()
-unattributed_rows['action_date'] = pd.to_datetime(unattributed_rows['Action Date'])
-earnings_unattributed = unattributed_rows.resample('ME', on='action_date')['Action Earnings'].sum()
-unattributed_list = earnings_unattributed.to_list()
 
 fmt = '${x:,.0f}'
 tick = mtick.StrMethodFormatter(fmt)
 
 earnings_by_user_type = pd.pivot_table(df, values="Action Earnings", index=pd.Grouper(freq='ME', key="action_date"), columns=["login_status"], aggfunc='sum', fill_value=0)
-earnings_by_user_type['unattributed'] = unattributed_list
 
 earnings_by_user_type_graph = earnings_by_user_type.plot(kind='line')
 earnings_by_user_type_graph.set_xlabel("Month")
---
+++ ticketmaster_by_user_type_yearly_plot.py
@@ -0,0 +1,30 @@
+# Run data prep script first!
+
+import pandas as pd
+import matplotlib.ticker as mtick
+
+df = pd.read_pickle('combined_df_left.pkl')
+df['action_date'] = pd.to_datetime(df['Action Date'])
+df["login_status"].fillna("unattributable", inplace = True)
+
+
+fmt = '${x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+
+earnings_by_user_type = pd.pivot_table(df, values="Action Earnings", index=pd.Grouper(freq='YE', key="action_date"), columns=["login_status"], aggfunc='sum', fill_value=0)
+
+earnings_by_user_type.loc['Total'] = earnings_by_user_type.sum()
+earnings_by_user_type = earnings_by_user_type.sort_values('Total', axis=1, ascending=True).drop('Total')
+earnings_by_user_type.index = pd.to_datetime(earnings_by_user_type.index).strftime('%Y')
+
+earnings_by_user_type_graph = earnings_by_user_type.plot(kind='bar', stacked=True)
+handles, labels = earnings_by_user_type_graph.get_legend_handles_labels()
+earnings_by_user_type_graph.legend(reversed(handles), reversed(labels), bbox_to_anchor=(1,1))
+earnings_by_user_type_graph.set_xlabel("Year")
+earnings_by_user_type_graph.set_ylabel("Total Earnings")
+earnings_by_user_type_graph.set_title('Yearly earnings from Ticketmaster by user type')
+earnings_by_user_type_graph.yaxis.set_major_formatter(tick)
+
+earnings_by_user_type_figure = earnings_by_user_type_graph.get_figure()
+earnings_by_user_type_figure.set_tight_layout(True)
+earnings_by_user_type_figure.savefig('ticketmaster-by-user-type-year.png')
